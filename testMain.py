31786ad6fb4c207844ade2bbaf58a2b191f345e4
Baz1ngaa <danila.buyval@gmail.com>
2023-03-27 19:14:53 +0200

    new

--- testMain.py
+++ testMain.py
@@ -33,8 +33,9 @@ available_timeBreak = ["6:30","7:00", "7:30"]
 available_timeBreakWeeking = ["9:30","10:00", "10:30", "11:00","11:30"]
 available_timeLaunch = ["12:30" , "13:00","13:30",  "14:00", "14:30", "15:00"]
 global wdw
-wdw=13
-available_timeEvening = ["18:00", "18:30","19:00", "19:30", "20:00",f"19:{wdw}"]
+wdw=16
+wdw2=18
+available_timeEvening = ["18:00", "18:30","19:00", "19:30", "20:00",f"19:{wdw}", f"19:{wdw2}"]
 available_YesOrNot = ["Да","Нет"]
 available_Ready = ["Готово"]
 
@@ -903,6 +904,17 @@ async def timeMessageEvening(dp: Dispatcher):
                 for person in usersLogin:
                         for oneMan in person:
                             await dp.bot.send_message(oneMan, f"Время ужинать, {nettName[Zahle]} ! Приятного аппетита!💗✨ \nНажми /ready , когда покушаешь")
+        if(currentime.hour==19 and currentime.minute==wdw2):
+            
+            sql.execute(f"SELECT login FROM profileTel WHERE timeEveningHour= 19 ")
+            usersLoginHour=sql.fetchall()
+            sql.execute(f"SELECT login FROM profileTel WHERE timeEveningMinute= {wdw2} ")
+            usersLogin=sql.fetchall()
+            usersLogin=list((Counter(usersLoginHour) & Counter(usersLogin)).elements())
+            if(usersLogin != None):
+                for person in usersLogin:
+                        for oneMan in person:
+                            await dp.bot.send_message(oneMan, f"Время ужинать, {nettName[Zahle]} ! Приятного аппетита!💗✨ \nНажми /ready , когда покушаешь")
         
     if(currentdate.weekday()==5 or currentdate.weekday()== 6 ):
         if(currentime.hour==18 and currentime.minute==0):
@@ -979,6 +991,7 @@ def schedule_jobs():
     #breakfastHour, breakfastMinute,launchHour,launchMinute,eveningHour, eveningMinute,breakfastWeekingHour, breakfastWeekingMinute,launchWeekingHour,launchWeekingMinute,eveningWeekingHour, eveningWeekingMinute=sql.fetchone()
     scheduler.add_job(timeMessageEvening, 'cron', minute=0,  second= 20, args=(dp,))
     scheduler.add_job(timeMessageEvening, 'cron', minute=wdw, second= 20, args=(dp,))
+    scheduler.add_job(timeMessageEvening, 'cron', minute=wdw2, second= 20, args=(dp,))
     scheduler.add_job(AllowNull, 'cron',hour=1, minute=30, args=())
     
 
