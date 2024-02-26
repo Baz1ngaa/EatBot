3ee4a164587273637d5cc5ce22d6385657ce4563
Baz1ngaa <danila.buyval@gmail.com>
2024-02-26 12:02:05 +0100

    w

--- NullGive.py
+++ NullGive.py
@@ -18,7 +18,8 @@ import pytz
 db=sqlite3.connect('EatTest.db')
 sql=db.cursor()
 sql.execute("SELECT login FROM profileTel")
-allLogins = sql.fetchone()  
+allLogins = sql.fetchall()  
+print(allLogins)
 for peopleMan in allLogins:
     sql.execute(f'UPDATE profileTel SET BFallowed = 0 WHERE login = "{peopleMan}"')
     db.commit()
