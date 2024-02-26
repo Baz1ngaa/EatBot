ae1aba2c3e8640075fb505dd510c3cfa10c9a348
Baz1ngaa <danila.buyval@gmail.com>
2024-02-26 12:36:58 +0100

    q

--- testOneGive.py
+++ testOneGive.py
@@ -1,3 +1,4 @@
+
 import logging
 from os import getenv
 from sys import exit
@@ -13,7 +14,7 @@ from collections import Counter
 import pytz
 
 
-db=sqlite3.connect('EatYuliia.db')
+db=sqlite3.connect('EatTest.db')
 sql=db.cursor()
 sql.execute("SELECT login FROM profileTel")
 allLogins = sql.fetchall()  
@@ -26,9 +27,10 @@ for i in range(len(allLogins)):
     db.commit()
     sql.execute(f'UPDATE profileTel SET EVallowed = 1 WHERE login = "{allLogins[i][0]}"')
     db.commit()
-    
+    print("ready")
 sql.execute("SELECT Name FROM Allowed") 
 allNameAllowed=sql.fetchone()
 for oneName in allNameAllowed:
     sql.execute(f'UPDATE Allowed SET Allowed = 1 WHERE Name = "{oneName}"')
     db.commit()
+         
