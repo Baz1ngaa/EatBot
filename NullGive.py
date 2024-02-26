341baa7609c2c82593e4c3a78ae9e685623b3118
Baz1ngaa <danila.buyval@gmail.com>
2024-02-26 11:45:36 +0100

    w

--- NullGive.py
+++ NullGive.py
@@ -1,27 +1,21 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
-from aiogram.dispatcher.filters import Text 
-from aiogram import Dispatcher, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
 import logging
 from os import getenv
 from sys import exit
-import aiogram.utils.markdown as fmt
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import BotCommand
+
 import asyncio
 import sqlite3
 import time
 import datetime
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
 from datetime import date
 import random
 from collections import Counter
 import pytz
 
 
-db=sqlite3.connect('EatYuliia.db')
+db=sqlite3.connect('EatTest.db')
 sql=db.cursor()
 sql.execute("SELECT login FROM profileTel")
 allLogins = sql.fetchone()  
